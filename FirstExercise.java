d6e200c929f20f88129e344ec74a1e43255546dc
José Gomes de Menezes <josegomesd6@gmail.com>
2024-01-13 00:02:58 -0300

    first commit

---
+++ FirstExercise.java
@@ -0,0 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class FirstExercise {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite a largura do terreno: ");
+        Float firstNumber = sc.nextFloat();
+        System.out.println("Digite o comprimento do terrono: ");
+        Float secondNumber = sc.nextFloat();
+        System.out.println("Digite o valor do metro quadrado: ");
+        Float squareMeter = sc.nextFloat();
+
+        Float area = firstNumber * secondNumber;
+        Float price = area * squareMeter;
+
+        System.out.printf("AREA = %.2f%n", area);
+        System.out.printf("PRECO: = %.2f%n",price);
+
+        sc.close();
+    }
+}
