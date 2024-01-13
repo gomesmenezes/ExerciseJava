efcbe09f498119f7cc01346eda9bbfbed8504d50
José Gomes de Menezes <josegomesd6@gmail.com>
2024-01-13 01:19:29 -0300

    two exercises completed

---
+++ Bhaskara.java
@@ -0,0 +1,33 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Bhaskara {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro número: ");
+        Double a = sc.nextDouble();
+
+        System.out.println("Digite o segundo número: ");
+        Double b = sc.nextDouble();
+
+        System.out.println("Digite o terceiro número: ");
+        Double c = sc.nextDouble();
+
+        Double delta = b * b - 4.0 * a * c;
+
+        if (delta < 0.00 || a == 0.0 ) {
+            System.out.println("Impossivel de calcular");
+        }
+        else {
+            Double r2 = -(b - Math.sqrt(delta)) / (2.0 * a);
+            Double r1 = -(b + Math.sqrt(delta)) / (2.0 * a);
+            System.out.printf("R1: %.5f%n", r1);
+            System.out.printf("R2: %.5f%n", r2);
+            
+        }
+
+        sc.close();
+    }
+}
---
+++ Telecommunications.java
@@ -0,0 +1,30 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Telecommunications {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Double standardPlan = 50.0;
+        Double minutes = 100.0;
+        Double priceExceed = 2.0;
+
+        System.out.println("Quantidade de minutos consumidas: ");
+        Double minutesConsumed = sc.nextDouble();
+
+        if (minutesConsumed > minutes) {
+            Double minutesUsed = minutesConsumed - minutes;
+            Double taxExceed = minutesUsed * priceExceed;
+            Double updateStandardPlan = standardPlan + taxExceed;
+            System.out.printf("Minutos excedidos da franquia: %.0f%n", minutesUsed);
+            System.out.printf("Valor das taxa: R$%.2f%n", taxExceed);
+            System.out.printf("Valor total: R$%.2f%n", updateStandardPlan);
+        }
+        else {
+            System.out.printf("Valor a pagar: R$%.2f%n", standardPlan);
+        }
+
+        sc.close();
+    }
+}
