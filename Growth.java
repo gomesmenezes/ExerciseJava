bdd3b68b6985d63eb8c99eea9d3972364bf9aad1
José Gomes de Menezes <josegomesd6@gmail.com>
2024-01-13 01:39:14 -0300

    One more

---
+++ Growth.java
@@ -0,0 +1,27 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Growth {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        while (x != y) {
+            if (x > y) {
+                System.out.println("A ordem esta decrescente ");
+            } 
+            else if (x < y) {
+                System.out.println("A ordem esta crescente ");
+            }
+
+            x = sc.nextInt();
+            y = sc.nextInt();
+
+        } 
+
+        sc.close();
+    }
+}
